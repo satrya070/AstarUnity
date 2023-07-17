22a250e2e7edb4940f1748aea53f17c9d0376387
satrya070 <satrya@spotr.ai>
2023-07-18 00:00:19 +0200

    generate all neighbor positions, next change g&f val to doble

--- Astar.cs
+++ Astar.cs
@@ -34,11 +34,18 @@ public class Program
 		//Console.WriteLine(pQueue[(0, 0)]);
 		
 		Console.WriteLine(heuristic((0, 0), (3, 7)));
+		
+		// --------------------//
+		node_a = new Node((0, 0), (0, 0), 3, 7);
+		var node_b = new Node((7, 2), (0, 0), 3, 7);
+		
+		expand_node(node_a, node_b);
 	}
 	
-	int expand_node(KeyValuePair<(int, int), int> current_node, KeyValuePair<(int, int), int> end_node, Dictionary<(int, int), int> pQueue)
+	static int expand_node(Node current_node, Node end_node)
 	{
-		// get all valid neighbors
+		// get all neighbors
+		// filter invalid neighbors
 		// runs for each neibors
 			// if node is goal stop
 			// compute f: g+h gets the eucleudian distance between neighbor_node and end_node
@@ -46,6 +53,20 @@ public class Program
 			// 
 		// checks if 
 		//(int x_1, int y_1) = pQueue[current_node];
+		// -----------------------------//
+		
+		var neighbors = get_neighbors((5, 7));
+		
+		Console.WriteLine("gathering all neighbors");
+		//neighbors.ForEach(delegate((int, int) x) { Console.WriteLine(x); });
+		
+		foreach((int, int) neighbor_position in neighbors)
+		{
+			// if node == end_node: stop
+			float neighbor_g_value =  current_node.g_value + 1;
+			float neighbor_f_value = neighbor_g_value + heuristic(neighbor_position, current_node.position);
+			neighbor = new Node(neighbor_position, current_node.position, current_node.g_value + 1, neighbor_f_value);
+		}
 		
 		return 8;
 	}
@@ -60,11 +81,32 @@ public class Program
 		return euclidian_distance;
 	}
 	
+	static List<(int, int)> get_neighbors((int, int) base_node_position)
+	{
+		(int pos_x, int pos_y) = base_node_position;
+		
+		IEnumerable<int> neighbors_x_coords = Enumerable.Range(pos_x - 1, 3);
+		IEnumerable<int> neighbors_y_coords = Enumerable.Range(pos_y - 1, 3);
+		
+		var neighbors = new List<(int, int)>();
+		foreach (var x in neighbors_x_coords)
+		{
+			foreach(var y in neighbors_y_coords)
+			{
+				neighbors.Add((x, y));
+			}
+		}
+		
+		neighbors.Remove((pos_x, pos_y));
+		
+		return neighbors;
+	}
+	
 	public class Node
 	{
 		public (int, int) position { get; set; }
-		public int g_value = 9999;
-		public int f_value = 9999;
+		public double g_value = 9999;
+		public double f_value = 9999;
 		public (int, int) previous_node { get; set; }
 		
 		public Node((int, int) Position, (int, int) Prev, int G_value, int F_value)
