f5a2db2424410f73d12631eac4936a579c2a61ee
satrya070 <satrya@spotr.ai>
2023-07-19 00:25:12 +0200

    added node expand!

--- Astar.cs
+++ Astar.cs
@@ -4,45 +4,36 @@ using System.Linq;
 					
 public class Program
 {
+	static Node end_node = new Node((7, 2), null, 3);
+	static List<Node> OpenPriorityQueue = new List<Node>();
+
 	public static void Main()
 	{
 		// position by 
-		//var open = new Dictionary<(int, int), Node>();
 		var closed = new Dictionary<(int, int), Node>();
 		
-		//var pQueue = new Dictionary<(int, int), int>();
-		var pQueue = new List<Node>();
+		OpenPriorityQueue.Add(new Node((0, 0), null, 0));
 		
-		var node_a = new Node((0, 0), (1, 3), 0, 10);
-		node_a.g_value = 9;
-		Console.WriteLine(node_a.g_value);
+		var sorted_list = OpenPriorityQueue.OrderBy(node => node.g_value).ToList();
+		//Console.WriteLine(sorted_list.Count);
 		
-		pQueue.Add(new Node((0, 0), (0, 0), 3, 7));
-		pQueue.Add(new Node((1, 1), (0, 0), 1, 4));
-		pQueue.Add(new Node((2, 2), (1, 1), 5, 9));
+		Node popped_node = sorted_list.First();
+		//Console.WriteLine(smallest.position);
 		
-		//var sorted_dict = pQueue.OrderBy(item => item.Value).ToDictionary(item => item.Key, item => item.Value);
-		//KeyValuePair<(int, int), int> pop = sorted_dict.First();
 		
-		var sorted_list = pQueue.OrderBy(node => node.g_value).ToList();
-		Console.WriteLine(sorted_list.Count);
-		
-		Node smallest = sorted_list.First();
-		
-		Console.WriteLine(smallest.position);
-		
-		//Console.WriteLine(pQueue[(0, 0)]);
-		
-		Console.WriteLine(heuristic((0, 0), (3, 7)));
+		//Console.WriteLine(heuristic((0, 0), (3, 7)));
 		
 		// --------------------//
-		node_a = new Node((0, 0), (0, 0), 3, 7);
-		var node_b = new Node((7, 2), (0, 0), 3, 7);
+		var node_a = new Node((0, 0), (0, 0), 0);
+		//var node_b = new Node((7, 2), (0, 0), 3);
 		
-		expand_node(node_a, node_b);
+		expand_node(popped_node);
+		
+		//neighbors.ForEach(delegate((int, int) x) { Console.WriteLine(x); });
+		OpenPriorityQueue.ForEach(delegate(Node node) { Console.WriteLine(node.position); });
 	}
 	
-	static int expand_node(Node current_node, Node end_node)
+	static void expand_node(Node expanding_node)//, Node end_node)
 	{
 		// get all neighbors
 		// filter invalid neighbors
@@ -55,7 +46,7 @@ public class Program
 		//(int x_1, int y_1) = pQueue[current_node];
 		// -----------------------------//
 		
-		var neighbors = get_neighbors((5, 7));
+		var neighbors = get_neighbors(expanding_node.position);
 		
 		Console.WriteLine("gathering all neighbors");
 		//neighbors.ForEach(delegate((int, int) x) { Console.WriteLine(x); });
@@ -63,12 +54,16 @@ public class Program
 		foreach((int, int) neighbor_position in neighbors)
 		{
 			// if node == end_node: stop
-			float neighbor_g_value =  current_node.g_value + 1;
-			float neighbor_f_value = neighbor_g_value + heuristic(neighbor_position, current_node.position);
-			neighbor = new Node(neighbor_position, current_node.position, current_node.g_value + 1, neighbor_f_value);
+			double neighbor_g_value = expanding_node.g_value + 1;
+			double neighbor_f_value = neighbor_g_value + heuristic(neighbor_position, end_node.position);
+			var neighbor = new Node(neighbor_position, expanding_node.position, neighbor_g_value);
+			neighbor.f_value = neighbor_f_value;
+
+			Console.WriteLine($"{expanding_node.position} {neighbor.position} - {neighbor.g_value} - {neighbor.f_value}");
+			OpenPriorityQueue.Add(neighbor);
 		}
 		
-		return 8;
+		//return 8;
 	}
 	
 	static double heuristic((int, int) start_node, (int, int) end_node)
@@ -107,14 +102,13 @@ public class Program
 		public (int, int) position { get; set; }
 		public double g_value = 9999;
 		public double f_value = 9999;
-		public (int, int) previous_node { get; set; }
+		public (int, int)? previous_node { get; set; }
 		
-		public Node((int, int) Position, (int, int) Prev, int G_value, int F_value)
+		public Node((int, int) Position, (int, int)? Prev, double G_value)
 		{
 			this.position = Position;
 			this.previous_node = Prev;
 			this.g_value = G_value;
-			this.f_value = F_value;
 		}
 	}
 }
